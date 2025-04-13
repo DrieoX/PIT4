b978b71fea8378ca5c0fed4791a3e807c4a89562
Llyle Andrie Lingat <dotandrie.12@gmail.com>
2025-04-13 16:12:12 +0800

    Debug for deployment

--- Back/fastapi-backend/database.py
+++ Back/fastapi-backend/database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("postgresql://todoapp_58p7_user:UuHOCqgYsIgAljSN2ZTuB4YIPzXQdbi5@dpg-cvtmpo24d50c73ak73s0-a/todoapp_58p7")  # Use full URL from Render
+DATABASE_URL = os.getenv("postgresql://todoapp_58p7_user:UuHOCqgYsIgAljSN2ZTuB4YIPzXQdbi5@dpg-cvtmpo24d50c73ak73s0-a.oregon-postgres.render.com/todoapp_58p7")  # Use full URL from Render
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- Front/todo-app/src/axios.js
+++ Front/todo-app/src/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://your-backend-domain.com/api/', // Replace with your FastAPI backend URL
+  baseURL: 'https://pit4.onrender.com/api/', // Replace with your FastAPI backend URL
   headers: {
     'Content-Type': 'application/json',
     // Include other headers if necessary, e.g., Authorization
